aa10b9b68cffbf8324b328d46d4459afc61cff2c
hongyi <hongyi@wdai.com>
2017-08-03 16:06:54 +0800

    spring-boot

--- comhyspringboot/src/main/java/com/hy/SpringbootStart.java
+++ comhyspringboot/src/main/java/com/hy/SpringbootStart.java
@@ -2,6 +2,7 @@ package com.hy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @ComponentScan(basePackages = "com.hy")
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootStart {
 
     public static void main(String[] args) {
